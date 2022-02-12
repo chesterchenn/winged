a846ba55b0fae540961b5409c346192e8c2f3017
chesterchenn <chesterchenn@gmail.com>
2022-02-12 14:29:37 +0800

    feat: 速蛙签到执行失败

--- suwa/index.js
+++ suwa/index.js
@@ -4,15 +4,24 @@ import serverJ from './serverJ.js';
 const URL = 'https://m.sw13.icu/api_mweb/user/checkin';
 const SU_WA = process.env.SU_WA;
 
-const res = await fetch(URL, {
-  method: 'put',
-  headers: {
-    'Content-Type': 'application/json',
-    authorizationMweb: SU_WA,
-  },
-});
+try {
+  const res = await fetch(URL, {
+    method: 'put',
+    headers: {
+      'Content-Type': 'application/json',
+      authorizationMweb: SU_WA,
+    },
+  });
 
-const data = await res.json();
-serverJ('速蛙签到', data.message);
-
-console.log(data);
+  if (!res.ok) {
+    throw res;
+  }
+  const data = await res.json();
+  console.log(data);
+  serverJ('速蛙签到', data.message);
+} catch (err) {
+  console.error(err);
+  const status = err.status;
+  const statusText = err.statusText;
+  serverJ('速蛙签到执行失败', `${status} ${statusText}`);
+}
