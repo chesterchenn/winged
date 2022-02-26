7f7e15d510dc1adf32fe6208376fcd4a1451c4ea
chesterchenn <chesterchenn@gmail.com>
2022-02-26 22:23:21 +0800

    feat: 统计每日签到流量

--- suwa/index.js
+++ suwa/index.js
@@ -1,9 +1,11 @@
 import fetch from 'node-fetch';
 import serverJ from './serverJ.js';
+import fs from 'fs';
 
 const URL = 'https://m.sw13.icu/api_mweb/user/checkin';
 const INFO_URL = 'https://m.sw13.icu/api_mweb/user/info';
 const SU_WA = process.env.SU_WA;
+const RESULT = './result.txt';
 
 try {
   const data = await fetchCheckin();
@@ -14,6 +16,15 @@ try {
     serverJ('速蛙签到失败', `${data.message}\n\n流量剩余${info.data.unused_traffic}`);
   } else {
     serverJ('速蛙签到', `${data.data.message}\n\n流量剩余${info.data.unused_traffic}`);
+    const date = new Date();
+    const YYYY = date.getFullYear();
+    const MM = (date.getMonth() + 1).toString().padStart(2, '0');
+    const DD = date.getDate().toString().padStart(2, '0');
+    if (DD === '26' && fs.existsSync(RESULT)) {
+      fs.rmSync(RESULT);
+    }
+    const save = `${YYYY}-${MM}-${DD} ${data.data.message} 流量剩余${info.data.unused_traffic}\n`;
+    fs.appendFileSync(RESULT, save);
   }
 } catch (err) {
   console.error(err);
